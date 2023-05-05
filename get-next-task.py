c4c7c08fefb1c16d6bae828a99da420d0a633824
stho32 <stho32github@gmail.com>
2023-05-05 18:03:01 +0200

    A new task every hour

--- get-next-task.py
+++ get-next-task.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import random
+import argparse
+import time
 
 # Base URL for Trello API
 BASE_URL = 'https://api.trello.com/1'
@@ -35,7 +37,19 @@ def get_list_id(config, board_id, list_name):
     return None
 
 
-def get_tasks_in_list(config, list_id):
+def get_tasks(config):
+    board_id = get_board_id(config, config['board_name'])
+
+    if not board_id:
+        print(f"No board found with the name '{config['board_name']}'.")
+        return []
+
+    list_id = get_list_id(config, board_id, config['list_name'])
+
+    if not list_id:
+        print(f"No list found with the name '{config['list_name']}'.")
+        return []
+
     url = f"{BASE_URL}/lists/{list_id}/cards?key={config['API_KEY']}&token={config['TOKEN']}"
     response = requests.get(url)
     return response.json()
@@ -45,33 +59,41 @@ def select_random_task(tasks):
     return random.choice(tasks)
 
 
+def print_task(task):
+    print("\nYour task for the next hour:")
+    print("===================================")
+    print(f"{task['name']}")
+    print("===================================\n")
+
+
 def main():
+    parser = argparse.ArgumentParser(description='Trello task selector.')
+    parser.add_argument('--loop', action='store_true', help='Enable loop mode to select tasks every hour.')
+
+    args = parser.parse_args()
+
     config = load_config()
 
-    board_id = get_board_id(config, config['board_name'])
+    if args.loop:
+        while True:
+            tasks = get_tasks(config)
 
-    if not board_id:
-        print(f"No board found with the name '{config['board_name']}'.")
-        return
+            if not tasks:
+                print("No tasks found. Retrying in 1 hour.")
+            else:
+                random_task = select_random_task(tasks)
+                print_task(random_task)
 
-    list_id = get_list_id(config, board_id, config['list_name'])
+            time.sleep(3600)  # Sleep for 1 hour
+    else:
+        tasks = get_tasks(config)
 
-    if not list_id:
-        print(f"No list found with the name '{config['list_name']}'.")
-        return
+        if not tasks:
+            print("No tasks found.")
+            return
 
-    tasks = get_tasks_in_list(config, list_id)
-
-    if not tasks:
-        print(f"No tasks found in the list '{config['list_name']}'.")
-        return
-
-    random_task = select_random_task(tasks)
-
-    print("\nYour task for today:")
-    print("===================================")
-    print(f"{random_task['name']}")
-    print("===================================\n")
+        random_task = select_random_task(tasks)
+        print_task(random_task)
 
 
 if __name__ == "__main__":
